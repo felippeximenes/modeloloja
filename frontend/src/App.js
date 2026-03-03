baefff4568aa6a79c199626c7df8f0cac6f7ebe6
felippeximenes <felippelpximenes@outlook.com>
2026-03-03 11:35:47 -0300

    feat(router): add MercadoPago return routes (success, failure, pending)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import "@/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Toaster } from 'sonner';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
-import Home from './pages/Home';
-import Shop from './pages/Shop';
-import ProductDetail from './pages/ProductDetail';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
+import { Toaster } from "sonner";
+
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import ProductDetail from "./pages/ProductDetail";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+
+import PaymentSuccess from "./pages/PaymentSuccess";
+import PaymentFailure from "./pages/PaymentFailure";
+import PaymentPending from "./pages/PaymentPending";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
+
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -21,8 +28,14 @@ function App() {
             <Route path="/product/:id" element={<ProductDetail />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/checkout" element={<Checkout />} />
+
+            {/* MercadoPago Return Pages */}
+            <Route path="/payment/success" element={<PaymentSuccess />} />
+            <Route path="/payment/failure" element={<PaymentFailure />} />
+            <Route path="/payment/pending" element={<PaymentPending />} />
           </Routes>
         </main>
+
         <Footer />
         <Toaster position="top-right" richColors />
       </BrowserRouter>
